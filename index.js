50bb7088bdf8ae7cefa41a0d8627228987e3485d
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-11-13 20:35:22 +0700

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json") //define Config
 //Fires when the Bot has started
 client.on("ready", () => {
     radioexecuteadmin(client); //call the function
-    client.user.setActivity("Bot host in replit.com/@kirigizakazuto/Waitingroom-Bot", { type: "PLAYING" }); //set Bot activity 
+    client.user.setActivity("Bot host in replit.com/@kirigizakazutov/Waitingroom-Bot", { type: "PLAYING" }); //set Bot activity 
     console.log(`${client.user.username} ready!`); //Log that the Bot is ready
 });
 //recognise if someone joins the channel
